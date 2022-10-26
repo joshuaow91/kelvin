aaa1de6fe7f60f982c5bc0e8e008a5982ae6f22f
joshuaow91 <113389785+joshuaow91@users.noreply.github.com>
2022-10-25 20:20:56 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,80 @@
+// varKelvin is the variable and constant name = 293
+var Kelvin = '293';
+// console.log(Kelvin) used to debug
+
+var Celsius = '273';
+// console.log(Celsius) used to debug
+
+// Combined using a String Concatenation to find the difference
+Kelvin - Celsius
+console.log(Kelvin - Celsius); 
+
+var Celsius = '20';
+
+// Provided Equation
+Fahrenheit = Celsius * (9/5) + 32
+
+// used to solve with console log
+console.log(Fahrenheit = Celsius * (9/5) + 32);
+
+var Fahrenheit = '68';
+
+//  Used a Math object to round number down
+console.log(Math.floor(Fahrenheit));
+//68
+
+var Fahrenheit = '68';
+
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
+
+// What's 0 Kelvin in Fahrenheit?
+var Kelvin = '0';
+
+var Celsius = '273';
+
+console.log(Kelvin - Celsius); 
+
+var Celsius = '-273';
+
+console.log(Fahrenheit = Celsius * (9/5) + 32);
+
+var Fahrenheit = '-459.40000000000003';
+
+console.log(Math.floor(Fahrenheit));
+
+var Fahrenheit = '-460';
+
+console.log(`The temperature is ${Fahrenheit} degrees Fahrenheit.`);
+
+// Extra Practice
+
+var Celsius = '273';
+
+console.log(Newton = Celsius * (33/100));
+
+var Newton = '90.09';
+
+console.log(Math.floor(Newton));
+
+var Newton = '90';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
